4577703086232570cdd92b0d23675805d14a6f73
huangjie4 <1364045672@qq.com>
2024-06-27 15:44:56 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Vant)
+app.mount('#app')
